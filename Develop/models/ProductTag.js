a724021ac1e05236c0f4c38c319e9abb5d785ca6
Mohammed Samod <mohammedlee27@gmail.com>
2021-11-12 14:45:05 -0600

    updated code

---
+++ Develop/models/ProductTag.js
@@ -0,0 +1,43 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection');
+
+class ProductTag extends Model {}
+
+ProductTag.init(
+  {
+    // define columns
+    id: {
+      type: DataTypes.INTEGER, 
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      // references the product model's id 
+      references: {
+        model: 'product',
+        key: 'id'
+      }
+    },
+    // defines tag_id column
+    tag_id: {
+      type: DataTypes.INTEGER,
+      // references the tag model's id 
+      references: {
+        model: 'tag',
+        key: 'id'
+      }
+    }    
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
+);
+
+module.exports = ProductTag;
