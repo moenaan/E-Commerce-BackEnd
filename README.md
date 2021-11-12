9056ce1c4508fdbb9f2eca3c724afc7e69f39f3a
Mohammed Samod <mohammedlee27@gmail.com>
2021-11-12 17:06:19 -0600

    update code

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# E-Commerce-BackEnd
+# E-Commerce-BackEnd\
+
+ ![license](https://img.shields.io/badge/License-MIT-brightgreen)
+
+ ## Table of Contents
+ * [Description](#Description)
+ * [Installation](#Installation)
+ * [Usage](#Usage)
+ * [Contributing](#Contributing)
+ * [Tests](#Tests)
+ * [License](#License)
+ * [Questions](#Questions)
+
+ ## Description
+    This project is a back end application for an e-commerce site.
+        
+ ## Installation
+ You can fork or download the repo from Github and you'll need Node.JS installed on your machine (You can type `node -v` in your terminal), if you don't have it you will need to install it first. This application also requires Express.js, and Sequelize. To connect to the database run mysql -u root -p and enter password from .env file. Then source the schema.sql. To seed the file run npm run seed. Finally to connect to the server run npm start.
+
+ ## Usage
+ This application allows the user to view products, categories and tags and edit, add, and delete them.
+
+ ## Contributing
+ Mohammed Samod.
+
+ ## Tests
+ There are no test for this application.
+
+ ## Questions
+ If you have any questions you can message using GitHub or Email.
+ 
+ Find me on GitHub: https://github.com/moenaan
+
+ Email: mohammedlee27@gmail.com
+
+ ## License
+ MIT
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,25 +7,62 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Category, attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag, attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+    .then(ProductData => res.json(ProductData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {id: req.params.id},
+    include: [
+      {
+        model: Category, attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag, attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+    .then(ProductData => {
+      if (!ProductData) {
+        res.status(404).json({ message: 'No product found with this id'}); 
+        return; 
+      }
+      res.json(ProductData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
+
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
-  Product.create(req.body)
+    Product.create({
+      product_name: req.body.product_name,
+      price: req.body.price,
+      stock: req.body.stock,
+      category_id: req.body.category_id,
+      tagIds: req.body.tag_id
+    })
+  
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -51,9 +88,7 @@ router.post('/', (req, res) => {
 router.put('/:id', (req, res) => {
   // update product data
   Product.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
+    where: {id: req.params.id,},
   })
     .then((product) => {
       // find all associated tags from ProductTag
@@ -91,6 +126,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+        id: req.params.id
+    }
+  })
+    .then(ProductData => {
+        if (!ProductData) {
+            res.status(404).json({ message: 'No product found with this id'});
+            return;
+        }
+        res.json(ProductData);
+  })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+  });
 });
 
 module.exports = router;
